59f6df8684ced8e637cdfbcf0dcd5f871cf1b8f2
Guilherme Montelo <montelocso@gmail.com>
2016-08-23 12:11:26 -0300

    Create TabSemRep.c

---
+++ TabSemRep.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int tab;
+
+void main(void){
+    printf("Digite o número da tabuada que deseja saber:");
+    scanf("%d",&tab);
+    printf("%d x0 = %d\n", tab, tab*0);
+    printf("%d x0 = %d\n", tab, tab*1);
+    printf("%d x0 = %d\n", tab, tab*2);
+    printf("%d x0 = %d\n", tab, tab*3);
+    printf("%d x0 = %d\n", tab, tab*4);
+    printf("%d x0 = %d\n", tab, tab*5);
+    printf("%d x0 = %d\n", tab, tab*6);
+    printf("%d x0 = %d\n", tab, tab*7);
+    printf("%d x0 = %d\n", tab, tab*8);
+    printf("%d x0 = %d\n", tab, tab*9);
+    printf("%d x0 = %d\n", tab, tab*10);
+}
